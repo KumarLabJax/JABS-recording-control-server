5360af1d555ee4b59cad8dfbc15bdda6574498df
Glen Beane <glen.beane@jax.org>
2019-08-06 11:43:20 -0400

    add more test for heartbeat endpoint

--- src/test/api/test_heartbeat.py
+++ src/test/api/test_heartbeat.py
@@ -67,6 +67,62 @@ class TestHeartbeat(BaseDBTestCase):
         response = self.client.post('/api/device/heartbeat', json=payload)
         self.assertStatus(response, 400)
 
+    #TODO for completeness we should have test for all required fields
+    def test_heartbeat_missing_required_field(self):
+        """ test heartbeat endpoint missing a required field (name) """
+        payload = {
+            'timestamp': datetime.utcnow().isoformat(),
+            'state': "IDLE",
+            'sensor_status': {
+                'camera': {
+                    'recording': False,
+                    'duration': 0,
+                    'fps': 0
+                }
+            },
+            'system_info': {
+                "uptime": 128324,
+                "load_1min": 0.66,
+                "load_5min": 0.23,
+                "load_15min": 0.12,
+                "total_ram": 8388608,
+                "free_ram": 7759462,
+                "free_disk": 1258291,
+                "total_disk": 2000000
+            }
+        }
+        response = self.client.post('/api/device/heartbeat', json=payload)
+        self.assertStatus(response, 400)
+
+    def test_heartbeat_bad_date_format(self):
+        """
+        test heartbeat endpoint with a bad datetime format (not ISO 8601)
+        """
+        payload = {
+            'timestamp': datetime.utcnow().strftime('%A %B %d %Y bad 1234 format'),
+            'name': "TEST_DEVICE",
+            'state': "IDLE",
+            'sensor_status': {
+                'camera': {
+                    'recording': False,
+                    'duration': 0,
+                    'fps': 0
+                }
+            },
+            'system_info': {
+                "uptime": 128324,
+                "load_1min": 0.66,
+                "load_5min": 0.23,
+                "load_15min": 0.12,
+                "total_ram": 8388608,
+                "free_ram": 7759462,
+                "free_disk": 1258291,
+                "total_disk": 2000000
+            }
+        }
+        response = self.client.post('/api/device/heartbeat', json=payload)
+        self.assertStatus(response, 400)
+
 
 if __name__ == '__main__':
     unittest.main()
