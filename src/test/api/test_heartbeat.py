0b3974a8cfded27ebce646ec7a405be337dc6e29
Glen Beane <glen.beane@jax.org>
2019-08-06 11:22:46 -0400

    add a test for a the heartbeat endpoint with a bad device state

--- src/test/api/test_heartbeat.py
+++ src/test/api/test_heartbeat.py
@@ -10,11 +10,10 @@ LOGGER = get_module_logger()
 
 
 class TestHeartbeat(BaseDBTestCase):
+    """ tests for the heartbeat endpoint """
 
     def test_heartbeat(self):
-        """
-        test the heartbeat endpoint
-        """
+        """ test the heartbeat endpoint """
         payload = {
             'timestamp': datetime.utcnow().isoformat(),
             'name': "TEST-DEVICE",
@@ -40,6 +39,33 @@ class TestHeartbeat(BaseDBTestCase):
 
         response = self.client.post('/api/device/heartbeat', json=payload)
         self.assertStatus(response, 204)
+
+    def test_heartbeat_bad_state(self):
+        """ test heartbeat endpoint with a bad device state """
+        payload = {
+            'timestamp': datetime.utcnow().isoformat(),
+            'name': "TEST-DEVICE",
+            'state': "BAD_STATE",
+            'sensor_status': {
+                'camera': {
+                    'recording': False,
+                    'duration': 0,
+                    'fps': 0
+                }
+            },
+            'system_info': {
+                "uptime": 128324,
+                "load_1min": 0.66,
+                "load_5min": 0.23,
+                "load_15min": 0.12,
+                "total_ram": 8388608,
+                "free_ram": 7759462,
+                "free_disk": 1258291,
+                "total_disk": 2000000
+            }
+        }
+        response = self.client.post('/api/device/heartbeat', json=payload)
+        self.assertStatus(response, 400)
 
 
 if __name__ == '__main__':
